a6d09c8c6c84f58e6213476bd3a9488b68df2d9a
lorematteo <loremattteo@gmail.com>
2025-01-18 18:12:00 +0100

    evol(sponsors): adding a new sponsor

--- frontend/src/const/sponsors.tsx
+++ frontend/src/const/sponsors.tsx
@@ -1,14 +1,18 @@
 export const SPONSORS = [
   {
-    img: '/brands/hardhat-logo.svg',
-    href: 'https://hardhat.org/',
+    img: '/brands/pinata-logo.svg',
+    href: 'https://pinata.cloud/',
   },
   {
     img: '/brands/ethereum-logo.svg',
     href: 'https://ethereum.foundation/',
   },
   {
-    img: '/brands/pinata-logo.svg',
-    href: 'https://pinata.cloud/',
+    img: '/brands/hardhat-logo.svg',
+    href: 'https://hardhat.org/',
+  },
+  {
+    img: '/brands/opensea-logo.svg',
+    href: 'https://opensea.io/',
   },
 ];
