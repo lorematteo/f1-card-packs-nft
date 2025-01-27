3e855c1b26083352c4c3f87fae23773e652f5fb8
lorematteo <loremattteo@gmail.com>
2025-01-27 11:08:58 +0100

    chore(files): delete unused alchemy config

--- frontend/src/web3/alchemy.ts
+++
@@ -1,8 +0,0 @@
-import { Alchemy, Network } from "alchemy-sdk";
-
-const config = {
-  apiKey: process.env.API_URL,
-  network: Network.ETH_SEPOLIA,
-}
-
-export const alchemy = new Alchemy(config);
